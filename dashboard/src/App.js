7e40074b6ad0db6effcf1c4e9fb3c096988dcfa3
David Muindi <muindidiego@gmail.com>
2023-10-27 18:22:26 +0300

    commit

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,22 +1,36 @@
-import React from 'react'; // Import React
-import logo from './logo.jpg'; // Import logo
-import './App.css'; // Import CSS styles
-
+import React from 'react';
+import logo from './logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+import Notifications from './Notifications'; // Import Notifications component
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-header-text">School dashboard</h1>
+        <div className="headerWrapper">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-header-text">School dashboard</h1>
+        </div>
       </header>
       <div className="App-body">
         <p className="App-body-text">Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <button type="submit">OK</button>
       </div>
-      <div className="App-footer">
-        <p className="App-footer-text">Copyright 2020 - Holberton School</p>
-      </div>
+      <Footer isIndex={true} />
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2019 - Holberton School</p>
+    </footer>
   );
 }
 
--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Notifications from './Notifications'; // Import the Notifications component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Notifications /> {/* Add the Notifications component here */}
   </React.StrictMode>
 );
 
@@ -15,3 +17,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
