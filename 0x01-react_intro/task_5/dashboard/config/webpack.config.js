5d565abd18052de9b99f5221ad52946b3d1c37f9
David Muindi <muindidiego@gmail.com>
2023-10-27 17:14:44 +0000

    Tasks

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+};
+
---
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -0,0 +1,4 @@
+// Targeting the 'app' element in the HTML and adding your application logic.
+const appElement = document.getElementById('app');
+appElement.textContent = 'Hello, World!';
+
