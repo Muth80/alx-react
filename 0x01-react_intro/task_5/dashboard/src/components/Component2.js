30eb9c91f1f3a0b03382024268997b836022c836
David Muindi <muindidiego@gmail.com>
2024-03-08 18:02:11 +0300

    Task

---
+++ 0x01-react_intro/task_5/dashboard/src/components/Component2.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Component2 = () => {
+  return <div>Hello from Component2!</div>;
+};
+
+export default Component2;
--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import Component1 from './components/Component1';
 import Component2 from './components/Component2';
 
-const App = () => <div>Hello, React with Webpack!</div>;
+const App = () => {
+  return (
+    <div>
+      <h1>React Intro</h1>
+      <Component1 />
+      <Component2 />
+    </div>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
